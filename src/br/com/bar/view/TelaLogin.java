0b3928922128ab33ddc5dcea89ea2e98a58d2545
Elias Santana <eliasantanasilva@gmail.com>
2018-12-23 09:51:37 -0300

    Versão 1.7.6 - CORREÇÃO FINAL - REV_03 REV_031

--- src/br/com/bar/view/TelaLogin.java
+++ src/br/com/bar/view/TelaLogin.java
@@ -186,7 +186,7 @@ public class TelaLogin extends javax.swing.JFrame {
         lblCnpjEmpresa.setBounds(20, 270, 260, 14);
 
         lblVersao1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lblVersao1.setText("Versão 1.7.4 - REV4");
+        lblVersao1.setText("Versão 1.7.6 - REV4_FINAL");
         jPanel1.add(lblVersao1);
         lblVersao1.setBounds(60, 290, 170, 20);
 
